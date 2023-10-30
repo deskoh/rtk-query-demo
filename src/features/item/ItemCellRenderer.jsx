e1d35a0df620715fddd2e8f5ceaef774fc6311bf
deskoh <deskoh.sg@gmail.com>
2023-10-30 20:36:37 +0800

    combine dispatch

--- src/features/item/ItemCellRenderer.jsx
+++ src/features/item/ItemCellRenderer.jsx
@@ -2,13 +2,16 @@ import { useDispatch } from 'react-redux';
 import { deleteOrderItemAction } from '../item/itemApi';
 import { updateOrderAction } from '../order/orderApi';
 
+const deleteItemAction = (orderId, itemId) => (dispatch) => {
+  dispatch(deleteOrderItemAction(orderId, itemId));
+  // Update parent order to mark as dirty
+  dispatch(updateOrderAction(orderId));
+}
+
 const ItemCellRenderer = ({ data }) => {
   const dispatch = useDispatch();
   const deleteItem = async () => {
-    dispatch(deleteOrderItemAction(data._orderId, data.id));
-
-    // Update parent order to mark as dirty
-    dispatch(updateOrderAction(data._orderId));
+    dispatch(deleteItemAction(data._orderId, data.id));
   };
 
   return (
